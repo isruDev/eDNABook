b50fff2f4748b7f9c98238f355124277bec2c3b8
isruDev <219510898+isruDev@users.noreply.github.com>
2026-03-17 19:27:23 -0700

    fix: vitest alias regex + manifest icon purpose split

--- vitest.config.js
+++ vitest.config.js
@@ -8,10 +8,10 @@ export default defineConfig({
     globals: false,
   },
   resolve: {
-    alias: {
-      '../lib/html5-qrcode.min.js': resolve('./node_modules/html5-qrcode/html5-qrcode.min.js'),
-      '../lib/papaparse.min.js': resolve('./node_modules/papaparse/papaparse.min.js'),
-      '../lib/sql-wasm.js': resolve('./node_modules/sql.js/dist/sql-wasm.js'),
-    },
+    alias: [
+      { find: /.*\/lib\/html5-qrcode\.min\.js$/, replacement: resolve('./node_modules/html5-qrcode/esm/index.js') },
+      { find: /.*\/lib\/papaparse\.min\.js$/, replacement: resolve('./node_modules/papaparse/papaparse.min.js') },
+      { find: /.*\/lib\/sql-wasm\.js$/, replacement: resolve('./node_modules/sql.js/dist/sql-wasm.js') },
+    ],
   },
 });
